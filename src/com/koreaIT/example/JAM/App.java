0c6edffcc3563ce2c87afe700df86f13678e2029
fbtjdwls04 <fbtjdwls04@gmail.com>
2023-10-13 14:18:49 +0900

    게시물 목록, 리스트 SecSql 적용

--- src/com/koreaIT/example/JAM/App.java
+++ src/com/koreaIT/example/JAM/App.java
@@ -3,9 +3,10 @@ package com.koreaIT.example.JAM;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 import com.koreaIT.example.JAM.util.DBUtil;
@@ -66,7 +67,7 @@ public class App {
 		if (cmd.equals("exit")) {
 			return -1;
 		}
-
+		// 게시물 작성
 		if (cmd.equals("article write")) {
 			System.out.println("==게시물 작성==");
 			System.out.printf("제목 : ");
@@ -85,56 +86,32 @@ public class App {
 			
 			System.out.printf("%d번 게시물이 생성되었습니다.\n", id);
 			
-		} else if (cmd.equals("article list")) {
+		} 
+		// 게시물 목록
+		else if (cmd.equals("article list")) {
 			System.out.println("==게시물 목록==");
-			PreparedStatement pstmt = null;
-			ResultSet rs = null;
-
-			ArrayList<Article> articles = new ArrayList<Article>();
-
-			try {
-				String sql = "SELECT *";
-				sql += " FROM article";
-				sql += " ORDER BY id DESC;";
-				System.out.println(sql);
-
-				pstmt = conn.prepareStatement(sql);
-
-				rs = pstmt.executeQuery(sql);
-
-				while (rs.next()) {
-					int id = rs.getInt("id");
-					String regDate = rs.getString("regDate");
-					String updateDate = rs.getString("updateDate");
-					String title = rs.getString("title");
-					String body = rs.getString("body");
-					Article article = new Article(id, regDate, updateDate, title, body);
-					articles.add(article);
-				}
-
-			} catch (SQLException e) {
-				System.out.println("에러 a.l: " + e);
-			} finally {
-				try {
-					if (rs != null && !rs.isClosed()) {
-						rs.close();
-					}
-					if (pstmt != null && !pstmt.isClosed()) {
-						pstmt.close();
-					}
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
+			List<Article> articles = new ArrayList<>();
+			
+			SecSql sql = new SecSql();
+			sql.append("SELECT * FROM article ORDER BY id DESC");
+			List<Map<String, Object>> list = DBUtil.selectRows(conn, sql);
+			
+			for(Map<String,Object> articleMap : list) {
+				articles.add(new Article(articleMap));
 			}
-			if (articles.size() == 0) {
-				System.out.println("게시글이 없습니다");
+			
+			if(articles.size() == 0) {
+				System.out.println("게시글이 없습니다.");
 				return 0;
 			}
-			System.out.println("번호   /   제목");
-			for (Article article : articles) {
-				System.out.printf("%4d   /   %s\n", article.id, article.title);
+			
+			System.out.println("번호/제목");
+			for(Article article : articles) {
+				System.out.printf("%d  /  %s  /  %s  /  %s\n",article.id, article.title, article.body, article.regDate);
 			}
-		} else if (cmd.startsWith("article modify")) {
+		} 
+		// 게시물 수정
+		else if (cmd.startsWith("article modify")) {
 			System.out.println("==게시물 수정==");
 			int id = Integer.parseInt(cmd.split(" ")[2]);
 
@@ -142,35 +119,16 @@ public class App {
 			String newTitle = sc.nextLine();
 			System.out.printf("새 내용 : ");
 			String newBody = sc.nextLine();
-
-			PreparedStatement pstmt = null;
-
-			try {
-
-				String sql = "UPDATE article";
-				sql += " SET updateDate = NOW(),";
-				sql += " title = '" + newTitle + "',";
-				sql += " `body` = '" + newBody + "'";
-				sql += " WHERE id = " + id + ";";
-
-				System.out.println(sql);
-
-				pstmt = conn.prepareStatement(sql);
-
-				pstmt.executeUpdate();
-
-			} catch (SQLException e) {
-				System.out.println("에러 : " + e);
-			} finally {
-				try {
-					if (pstmt != null && !pstmt.isClosed()) {
-						pstmt.close();
-					}
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			}
-			System.out.println(id + "번 글이 수정되었습니다");
+			
+			SecSql sql = new SecSql();
+			sql.append("UPDATE article");
+			sql.append("SET title = ?,",newTitle);
+			sql.append("body = ?,",newBody);
+			sql.append("updateDate = NOW()");
+			sql.append("WHERE id = ?",id);
+			
+			DBUtil.update(conn, sql);
+			System.out.printf("%d번 글이 수정되었습니다.\n",id);
 		}
 
 		return 0;
